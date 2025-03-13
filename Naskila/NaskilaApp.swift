acc9fd11da4bfb17e628be524a7a365ac3740ea7
Developer <Lepiozdyx@gmail.com>
2025-03-13 08:57:34 -0300

    Initial Commit

---
+++ Naskila/NaskilaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaskilaApp.swift
+//  Naskila
+//
+//  Created by Alex on 13.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct NaskilaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
